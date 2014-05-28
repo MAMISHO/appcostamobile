8adb77b7b9bec3e708d69faed7066d15af61dde3
MAMISHO <mamisho@hotmail.es>
2014-05-28 08:02:42 +0200

    inicio
    del proyecto

---
+++ source/App.js
@@ -0,0 +1,57 @@
+enyo.kind({
+	name: "App",
+	kind: "FittableRows", 
+	classes: "enyo-fit enyo-unselectable",
+	components: [
+      { kind:"Header" },
+      {   
+          kind: "Scroller",
+          name: "contentControl",
+          fit:true,
+          touch:true, 
+          thumb:true,
+          components:[
+            { 
+              name:"registerFormControl",
+              kind:"SampleForm" 
+            }
+          ]
+      },
+      { 
+        kind:"Footer",
+        onSubmit:"handleFormSubmit" 
+      }
+	],
+ 
+	create: function(){
+		  this.inherited(arguments);
+	},
+  handleFormSubmit:function(inSender,inEvent) {
+      //Access a kind method is as simple as hashing it's name and trigger the method.
+      this.payLoad = this.$.registerFormControl.getFormValues();
+      console.log(this.payLoad);
+      //This is a standard enyo.Ajax to post data. 
+      var ajax = new enyo.Ajax({
+          url: "http://localhost:8080/usuario/webresources/movil?usuario=edwin&clave=edwin",
+
+          //method:"POST",
+          //timeout:5000,
+          //contentType:"application/json"
+      });
+      // send parameters the remote service using the 'go()' method
+      //ajax.go(this.payLoad);
+      ajax.go();
+      // attach responders to the transaction object
+      ajax.response(this, "processResponse");
+      // handle error
+      ajax.error(this, "processError");
+  },
+  processResponse:function(inSender,inResponse) {
+      //To access the retrieved value just use inResponse;
+      console.log(inResponse);
+      alert("All Done");
+  },
+  processError:function(inSender,inResponse) {
+      alert("Failed");
+  } 
+});
---
+++ source/Header.js
@@ -0,0 +1,13 @@
+enyo.kind({
+    name: "Header",
+    kind: "Control",
+    components: [
+        {
+             kind: "onyx.Toolbar",
+             content:"AppCostal - Beta",
+             style:"text-align:center"
+        }
+    ]
+});      
+
+      
